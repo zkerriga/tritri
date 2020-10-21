a253c3312dae182b86c7cfc7bde3ad9326110322
zkerriga <daniil.63@yandex.ru>
2020-10-21 10:28:57 +0300

    Add icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:tritri/database/databaseHelper.dart';
-import 'package:tritri/models/human.dart';
 import 'package:tritri/myColors.dart';
 import 'package:tritri/pages/homePage.dart';
 
@@ -38,22 +37,4 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-/*
-    return ChangeNotifierProvider<HumansDataProvider>(
-      create: (context) => HumansDataProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'TriTri',
-        theme: ThemeData(
-          primarySwatch: MyColors.palette,
-          backgroundColor: MyColors.white,
-          textTheme: GoogleFonts.robotoTextTheme(
-            Theme.of(context).textTheme,
-          )
-        ),
-        home: HomePage(),
-    ),
-    );
-  }
- */
 }
