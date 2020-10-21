9402607240f2db11ea32639c45e6f2ca3abb1757
zkerriga <daniil.63@yandex.ru>
2020-10-21 11:12:31 +0300

    Add fist card style

--- lib/widgets/itemHuman.dart
+++ lib/widgets/itemHuman.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tritri/models/human.dart';
+import 'package:flutter_dash/flutter_dash.dart';
+import 'package:tritri/myColors.dart';
 
 class ItemHuman extends StatelessWidget {
   const ItemHuman({Key key}) : super(key: key);
@@ -12,6 +14,61 @@ class ItemHuman extends StatelessWidget {
     const String _catImageURL = 'https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png';
 
     return Container(
+      height: 100,
+      margin: const EdgeInsets.all(5.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 80,
+            width: 80,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                image: DecorationImage(
+                  image: NetworkImage(_catImageURL),
+                  fit: BoxFit.cover,
+                )
+            ),
+          ),
+          Container(
+            padding: EdgeInsetsDirectional.only(start: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    '${human.firstName} ${human.lastName}',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Skills: ${human.getSkillsString()}',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    'Hobbies: ${human.getHobbiesString()}',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+                  ),
+                ),
+                Dash(
+                  direction: Axis.horizontal,
+                  dashLength: 3,
+                  dashColor: MyColors.lightPink,
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+    );
+
+
+/*    return Container(
       width: 150,
       padding: const EdgeInsets.all(10.0),
       margin: const EdgeInsets.all(5.0),
@@ -60,6 +117,6 @@ class ItemHuman extends StatelessWidget {
             ),
         ],
       ),
-    );
+    );*/
   }
 }
