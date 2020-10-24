a0c0ce6afdbf29714c7cf6622326ce475c8c4c6d
zkerriga <daniil.63@yandex.ru>
2020-10-24 16:13:40 +0300

    Add norm scroll to humanCreatePage

--- lib/pages/createHumanPage.dart
+++ lib/pages/createHumanPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:tritri/database/databaseHelper.dart';
 import 'package:tritri/models/human.dart';
@@ -18,62 +17,24 @@ class CreateHumanPage extends StatelessWidget {
       ),
       backgroundColor: MyColors.white,
       resizeToAvoidBottomPadding: false,
-      body: CreateHumanForm(),
+      body: _CreateHumanForm(),
     );
   }
 }
 
-class CreateHumanForm extends StatefulWidget {
+class _CreateHumanForm extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => CreateHumanState();
+  State<StatefulWidget> createState() => _CreateHumanState();
 }
 
-class _PhotoBlock extends StatelessWidget {
-  static const String _catImageURL = 'https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png';
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      // crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 4,
-          child: Center(
-            child: CircleAvatar(
-              maxRadius: 500,
-              backgroundImage: NetworkImage(_catImageURL),
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(
-                  icon: Icon(Icons.file_download),
-                  onPressed: null
-              ),
-              IconButton(
-                  icon: Icon(Icons.camera),
-                  onPressed: null
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class CreateHumanState extends State {
+class _CreateHumanState extends State {
   final _formKey = GlobalKey<FormState>();
   String        _firstName = "_";
   String        _lastName = "_";
   String        _link = "_";
   List<String>  _skillsList = [];
   List<String>  _hobbiesList = [];
+  static const String _catImageURL = 'https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png';
 
   Widget build(BuildContext context) {
     final humanData = Provider.of<DataProvider>(context);
@@ -111,125 +72,136 @@ class CreateHumanState extends State {
     }
     return Form(
       key: _formKey,
-      child: Container(
-        width: _width,
-        height: _height,
-        padding: EdgeInsets.all(_width * 0.05),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: _PhotoBlock(),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              icon: Icon(Icons.person),
-                              hintText: 'Как зовут человека?',
-                              labelText: 'Имя',
-                            ),
-                            onSaved: (String value) {
-                              _firstName = value;
-                            },
-                            validator: _validateName,
+      child: ListView(
+        children: [
+          Container(
+            width: _width,
+            height: _height,
+            padding: EdgeInsets.all(_width * 0.05),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 4,
+                        child: Center(
+                          child: CircleAvatar(
+                            maxRadius: 500,
+                            backgroundImage: NetworkImage(_catImageURL),
                           ),
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              icon: Icon(Icons.person),
-                              hintText: 'Фамилия человека?',
-                              labelText: 'Фамилия',
-                            ),
-                            onSaved: (String value) {
-                              _lastName = value;
-                            },
-                            validator: _validateLastName,
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              icon: Icon(Icons.link),
-                              hintText: 'Ссылка для связи?',
-                              labelText: 'Ссылка',
-                            ),
-                            onSaved: (String value) {
-                              _link = value;
-                            },
-                            validator: _validateLink,
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                      Spacer(flex: 1,),
+                      Expanded(
+                        flex: 10,
+                        child: Container(
+                          // padding: const EdgeInsets.only(left: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  icon: Icon(Icons.person),
+                                  hintText: 'Как зовут человека?',
+                                  labelText: 'Имя',
+                                ),
+                                onSaved: (String value) {
+                                  _firstName = value;
+                                },
+                                validator: _validateName,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  icon: Icon(Icons.person),
+                                  hintText: 'Фамилия человека?',
+                                  labelText: 'Фамилия',
+                                ),
+                                onSaved: (String value) {
+                                  _lastName = value;
+                                },
+                                validator: _validateLastName,
+                              ),
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                  icon: Icon(Icons.link),
+                                  hintText: 'Ссылка для связи?',
+                                  labelText: 'Ссылка',
+                                ),
+                                onSaved: (String value) {
+                                  _link = value;
+                                },
+                                validator: _validateLink,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.star),
-                  hintText: 'Навыки человека?',
-                  labelText: 'Навыки',
                 ),
-                onSaved: (String value) => _skillsList = _toSkillsList(value),
-                validator: _validateLists,
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.star_border_purple500_sharp),
-                  hintText: 'Хобби человека?',
-                  labelText: 'Хобби',
+                Divider(),
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.star),
+                      hintText: 'Навыки человека?',
+                      labelText: 'Навыки',
+                    ),
+                    onSaved: (String value) => _skillsList = _toSkillsList(value),
+                    validator: _validateLists,
+                  ),
                 ),
-                onSaved: (String value) => _hobbiesList = _toSkillsList(value),
-                validator: _validateLists,
-              ),
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.star_border_purple500_sharp),
+                      hintText: 'Хобби человека?',
+                      labelText: 'Хобби',
+                    ),
+                    onSaved: (String value) => _hobbiesList = _toSkillsList(value),
+                    validator: _validateLists,
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: IconButton(
+                    icon: Icon(Icons.check),
+                    iconSize: _width * 0.2,
+                    color: MyColors.purple,
+                    onPressed: () {
+                      // Validate returns true if the form is valid, otherwise false.
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        Scaffold
+                            .of(context)
+                            .showSnackBar(SnackBar(
+                            backgroundColor: MyColors.lightPink,
+                            content: Text('Идёт сохранение..')
+                        ));
+                        humanData.addHuman(Human(
+                          _firstName,
+                          _lastName,
+                          _link,
+                          _skillsList,
+                          _hobbiesList,
+                        ));
+                        Navigator.pop(context);
+                      }
+                    },
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: SizedBox(),
-            ),
-            Expanded(
-              flex: 3,
-              child: IconButton(
-                icon: Icon(Icons.check),
-                iconSize: _width * 0.2,
-                color: MyColors.purple,
-                onPressed: () {
-                  // Validate returns true if the form is valid, otherwise false.
-                  if (_formKey.currentState.validate()) {
-                    _formKey.currentState.save();
-                    Scaffold
-                        .of(context)
-                        .showSnackBar(SnackBar(
-                        backgroundColor: MyColors.lightPink,
-                        content: Text('Идёт сохранение..')
-                    ));
-                    humanData.addHuman(Human(
-                      _firstName,
-                      _lastName,
-                      _link,
-                      _skillsList,
-                      _hobbiesList,
-                    ));
-                    Navigator.pop(context);
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+          SizedBox(
+            width: _width,
+            height: _height * 0.2,
+          ),
+        ],
       ),
     );
   }
